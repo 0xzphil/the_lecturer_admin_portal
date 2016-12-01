75bce16f6b986208784747c5f21c08d26f165626
fizzschool <fizz.uet@gmail.com>
2016-12-01 16:22:38 +0700

    Update base, session start cho 3 kiểu người dùng, thêm thư viện Guzzle Thêm khởi tạo của sinh viên

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
 use Auth;
 use Excel;
 use Session;
@@ -31,12 +32,14 @@ class UserController extends Controller
 
     public function redirectAfterLogin(){
         if(isset(Auth::user()->role)){
+            // create session variables
+            $this->sessionStart();
             if(Auth::user()->role == 'khoa'){
                 $nhan_vien_khoa =  Nhan_vien_khoa::where('user_id','=', Auth::user()->id)->firstOrFail();
-                $khoa_id =  $nhan_vien_khoa->khoa_id;
+                //$khoa_id =  $nhan_vien_khoa->khoa_id;
                 $khoa = Khoa::find($khoa_id);
                 Session::put('ten_khoa', $khoa->ten_khoa);
-                Session::put('khoa_id', $khoa_id);
+                //Session::put('khoa_id', $khoa_id);
                 Session::put('name', Auth::user()->name);
                //echo $khoa->ten_khoa;
                 return view('khoa2')->with('ten_khoa', $khoa->ten_khoa);
@@ -47,11 +50,24 @@ class UserController extends Controller
             }
             else if(Auth::user()->role == 'sinh_vien'){
                 $sinh_vien = Auth::user()->sinh_vien->user_id;
-                return view('sinh_vien.profile');
+                $bo_mon = BaseController::listBomon();
+                return view('sinh_vien.profile')->with('bo_mons',  $bo_mon);
             } else {
                 return view('');
             }
         }
         else return redirect('/login');
+    }
+
+    private function sessionStart()
+    {
+        # code...
+        $role = Auth::user()->role;
+        if( $role!= 'giang_vien'){
+            $khoa_id = Auth::user()->$role->khoa_id;
+        } else {
+            $khoa_id = Auth::user()->$role->bo_mon->khoa_id;
+        }
+        Session::put('khoa_id', $khoa_id);
     }
 }
