b8159ab36e1229716e429ae7d7135cafa2ffa969
fizzschool <fizz.uet@gmail.com>
2016-12-14 09:18:12 +0700

    Cập nhật seed, giao diện

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -17,9 +17,9 @@ class UserTableSeeder extends Seeder
     {
         // 1
         User::insert([
-        	'name'   => 'giangvien',
+        	'name'   => 'T.S Lê Đình Thanh',
             'password'   => Hash::make('giangvien'),
-            'email'      => 'khoa.uet@gmail.com', 
+            'email'      => 'giangvien.uet@gmail.com', 
             'role'       => 'giang_vien',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
@@ -35,7 +35,7 @@ class UserTableSeeder extends Seeder
         ]);
         // 3
         User::insert([
-            'name'   => 'Phi',
+            'name'   => 'Phi Nguyễn',
             'password'   => Hash::make('phiphi'),
             'email'      => 'phi.uet@gmail.com', 
             'role'       => 'giang_vien',
@@ -44,7 +44,7 @@ class UserTableSeeder extends Seeder
         ]);
         // 4
         User::insert([
-        	'name'   => 'Minh',
+        	'name'   => 'Nguyễn Văn Minh',
             'password'   => Hash::make('minhminh'),
             'email'      => 'minh.uet@gmail.com', 
             'role'       => 'giang_vien',
@@ -53,18 +53,18 @@ class UserTableSeeder extends Seeder
         ]);
         // 5
         User::insert([
-            'name'   => 'Loc',
-            'password'   => Hash::make('locloc'),
-            'email'      => 'loc.uet@gmail.com', 
+            'name'   => 'Đinh Tiến Lộc',
+            'password'   => Hash::make('123456'),
+            'email'      => '14020347@vnu.edu.vn', 
             'role'       => 'sinh_vien',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
         // 6
         User::insert([
-            'name'   => 'Cong',
-            'password'   => Hash::make('congcong'),
-            'email'      => 'cong.uet@gmail.com', 
+            'name'   => 'Hữu Công',
+            'password'   => Hash::make('123456'),
+            'email'      => '14020377@vnu.edu.vn', 
             'role'       => 'sinh_vien',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
